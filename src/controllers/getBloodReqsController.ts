8e6913918af0814bc87635a2128a987fa40ce84e
keplar-404 <shehabhossen76.services@gmail.com>
2024-02-09 21:42:02 +0000

    user get api added

--- src/controllers/getBloodReqsController.ts
+++ src/controllers/getBloodReqsController.ts
@@ -7,54 +7,14 @@ export default async function getBloodRequests(
 ) {
   try {
     // Retrieve all data using find method
-    const users = await UserSchema.find({ bloodReq: { $exists: true } });
-    let bloodRequests = await users.map((user) => { return user.bloodReq})[0]
+    const users = await UserSchema.find();
 
+    let userBloodReq = await users.filter((user) => user?.bloodReq[0]);
 
-// console.log(bloodRequests)
-
-    // Sort data by time
-    bloodRequests.sort((a, b) => {
-      // Check if a and b have valid time properties before comparing
-      if (a && b && a.time && b.time) {
-        return new Date(b.time).getTime() - new Date(a.time).getTime();
-      }
-      return 0;
-    });
-
-    // Transform time to human-readable format
-    const bloodRequestsTransformed = bloodRequests.map((data) => {
-      // Check if data.time is defined and not null
-      if (data.time !== undefined && data.time !== null) {
-        const dateString: string = data.time.toString();
-        const dateObject = new Date(dateString);
-
-        // Options for formatting the date, adjust as needed
-        const options: Intl.DateTimeFormatOptions = {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        };
-
-        // Convert to human-readable date
-        const humanReadableDate = dateObject.toLocaleDateString(
-          "en-US",
-          options
-        );
-
-        // Return the transformed data
-        return {
-          ...data.toObject(), // Convert Mongoose document to plain object
-          time: humanReadableDate,
-        };
-      }
-
-      return null; // Handle the case where time is undefined or null
-    }).filter((data) => data !== null); // Remove null values from the array
+    let bloodReq = userBloodReq.map((user) => user.bloodReq);
 
     res.status(200).json({
-      message: "sucessfully retrieved blood requests",
-      bloodRequests: bloodRequestsTransformed,
+      bloodRequests: bloodReq,
     });
   } catch (err) {
     next(err);
